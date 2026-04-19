67024a65055776802918549cdf5b0f5b347301f8
Kamil Gruca <kgruca7747@gmail.com>
2026-04-19 15:03:24 +0200

    feat(NEU-38): add base component for status badge

---
+++ app/components/app-status-badge/AppStatusBadge.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { cn } from '../../utils/tailwind.utils';
+import { type AppStatusBadgeProps, appStatusBadgeVariants } from '.';
+
+withDefaults(defineProps<AppStatusBadgeProps>(), {
+  color: 'default',
+  icon: undefined,
+  spinning: false,
+});
+</script>
+
+<template>
+  <span :class="cn(appStatusBadgeVariants({ color }))">
+    <Icon
+      v-if="icon"
+      :name="icon"
+      size="1.2rem"
+      :class="{ 'animate-spin': spinning }"
+      aria-hidden="true"
+    />
+    {{ label }}
+  </span>
+</template>
